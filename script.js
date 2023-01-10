e7c248e1e11edd0fefb20c492066ddcbebd7d31d
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-01-10 20:39:34 +0100

    Add actions on add del button

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const numberButtons = document.querySelectorAll('.number')
+let input = document.querySelector('.upper')
+let inputLower = document.querySelector('.lower')
+let firstValue
+let secoundValue
+let value
+const deleteButton = document.querySelector('.delete')
+const addButton = document.querySelector('.add')
+const resultButton = document.querySelector('.result')
 
+resultButton.addEventListener('click', ()=>{
+    console.log(firstValue)
+    secoundValue = value
+    console.log(secoundValue)
+})
+
+deleteButton.addEventListener('click', ()=>{
+    value = inputLower.innerHTML = ' '
+})
+addButton.addEventListener('click', ()=>{
+    input.innerHTML = value + ' + '
+    firstValue = value
+    value = inputLower.innerHTML = ' '
+
+})
 
 numberButtons.forEach(button =>{
     button.addEventListener('click', () =>{
-        console.log('Klicked!')
+        let inputValue = button.innerText
+        value = inputLower.innerHTML += inputValue
     })
 })
